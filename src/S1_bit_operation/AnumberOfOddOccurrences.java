54b5943d06a15ad63c52f341aa726ebbf37c4522
Echo-sly <hrbust_sly@163.com>
2021-07-10 23:26:59 +0800

    2021.7.10

--- src/S1_bit_operation/AnumberOfOddOccurrences.java
+++ src/S1_bit_operation/AnumberOfOddOccurrences.java
@@ -1,9 +1,10 @@
+
 package S1_bit_operation;
 
 public class AnumberOfOddOccurrences {
 
     //在一个数组中，只有一个数字出现奇数次，其余都出现偶数次，找到他！
-    public static int findOddOCcurrencesNumber(int[] arr) {
+    public static int findOddOccurrencesNumber(int[] arr) {
 
         int res = 0;
         for (int i = 0; i < arr.length; i++) {
@@ -12,11 +13,23 @@ public class AnumberOfOddOccurrences {
         return res;
     }
 
+    //在一个数组中，只有两个数字出现奇数次，其余都出现偶数次，找到他们！
+    public static void findTwoOddOccurrencesNumber(int[] arr){
+
+        int res = 0; //所有数亦或之后的结果即两个结果的亦或
+
+        for (int i = 0; i < arr.length; i++) {
+            res ^= arr[i];
+        }
+
+        int tmp = res&(-res);
+
+    }
     public static void main(String[] args) {
 
         int[] arr1 = {1,1,56,56,48,18,18,23,23};
 
-        System.out.println(findOddOCcurrencesNumber(arr1));
+        System.out.println(findOddOccurrencesNumber(arr1));
 
     }
 }
