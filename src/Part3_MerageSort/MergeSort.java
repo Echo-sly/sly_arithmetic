bc4506dd4806050135a6efc9fb22ba7fcc6d1c95
Echo-sly <hrbust_sly@163.com>
2021-08-13 21:44:43 +0800

    2021.8.13

--- src/Part3_MerageSort/MergeSort.java
+++ src/Part3_MerageSort/MergeSort.java
@@ -11,13 +11,23 @@ import java.util.Collection;
  */
 public class MergeSort {
 
+	public static void mergeSort1(int[] arr) {
+		if (arr == null || arr.length < 2) {
+			return;
+		}
+		process(arr, 0, arr.length - 1);
+	}
+
+	/**
+	 * 在arr数据上，L...R位置上排序
+	 */
 	public static void process(int[] arr, int L, int R) {
 		if (L == R) {
 			return;
 		}
-		int mid = L + ((R - L) >> 2);
-		process(arr, L, mid + 1);
-		process(arr, mid + 1, L);
+		int mid = L + ((R - L) >> 1);
+		process(arr, L, mid);
+		process(arr, mid + 1, R);
 		merge(arr, L, mid, R);
 	}
 
@@ -35,11 +45,51 @@ public class MergeSort {
 		while (p2 <= r) {
 			help[i++] = arr[p2++];
 		}
+		for (i = 0; i < help.length; i++) {
+			arr[l + i] = help[i];
+		}
 	}
 
-	public static void mergeSort2(int[] arr, int L, int R){
+	public static void mergeSort2(int[] arr) {
+		if (arr == null || arr.length < 2) {
+			return;
+		}
+		int N = arr.length;
+		// 步长
+		int mergeSize = 1;
+		while (mergeSize < N) {
+			// log N
+			// 当前左组的，第一个位置
+			int L = 0;
+			while (L < N) {
+				if (mergeSize >= N - L) {
+					break;
+				}
+				int M = L + mergeSize - 1;
+				int R = M + Math.min(mergeSize, N - M - 1);
+				merge(arr, L, M, R);
+				L = R + 1;
+			}
+			// 防止溢出
+			if (mergeSize > N / 2) {
+				break;
+			}
+			mergeSize *= 2;
+		}
+	}
 
-
+	public static void main(String[] args) {
+		int[] arr = {9, 8, 11, 1, 15, 49, 23, 26, 18, 59, 12, 45, 18, 2, 89, 3};
+		int[] arr2 = arr.clone();
+		mergeSort1(arr);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + ",");
+		}
+		System.out.println();
+		mergeSort2(arr2);
+		for (int i = 0; i < arr2.length; i++) {
+			System.out.print(arr2[i] + ",");
+		}
 
 	}
 
