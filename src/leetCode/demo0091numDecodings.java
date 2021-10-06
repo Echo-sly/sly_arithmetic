e55c532c74f44a4c598cf41eae7e6e8e22a97304
sly <hrbust_sly@163.com>
2021-10-06 22:43:22 +0800

    2021.10.6

---
+++ src/leetCode/demo0091numDecodings.java
@@ -0,0 +1,28 @@
+package leetCode;
+
+/**
+ * Demo demo0091numDecodings
+ *
+ * @author sunlianyu
+ * @date 10月06日 22:21
+ */
+public class demo0091numDecodings {
+    public int numDecodings(String s) {
+        final int length = s.length();
+        if(length == 0) return 0;
+        if(s.charAt(0) == '0') return 0;
+
+        int[] dp = new int[length+1];
+        dp[0] = 1;
+
+        for(int i=0;i<length;i++){
+            dp[i+1] = s.charAt(i)=='0'?0:dp[i];
+            if(i > 0 && (s.charAt(i-1) == '1' || (s.charAt(i-1) == '2' && s.charAt(i) <= '6'))){
+                dp[i+1] += dp[i-1];
+            }
+        }
+
+        return dp[length];
+    }
+
+}
