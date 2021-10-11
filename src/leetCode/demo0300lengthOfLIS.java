dba70bb3a0f90e731cabef264046cae697f806d9
sly <hrbust_sly@163.com>
2021-10-11 20:41:20 +0800

    2021.10.11

---
+++ src/leetCode/demo0300lengthOfLIS.java
@@ -0,0 +1,28 @@
+package leetCode;
+
+/**
+ * Demo demo0300lengthOfLIS
+ *
+ * @author sunlianyu
+ * @date 10月11日 20:40
+ */
+public class demo0300lengthOfLIS {
+    public int lengthOfLIS(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = 1;
+        int maxans = 1;
+        for (int i = 1; i < nums.length; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            maxans = Math.max(maxans, dp[i]);
+        }
+        return maxans;
+    }
+}
